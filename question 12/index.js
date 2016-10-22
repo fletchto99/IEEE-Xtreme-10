9a31fa20408778273194f5b34c1bcfcffe178cba
Matt Langlois <fletchto99@gmail.com>
2016-10-22 16:23:29 -0400

    Full adder almost fixed...

--- question 12/index.js
+++ question 12/index.js
@@ -1,15 +1,12 @@
 function processData(input) {
     var lines = input.split('\n');
-
+    lines.pop();
     let chars = lines[0].split(" ")[1].trim();
     let base = parseInt(lines[0].split(" ")[0]);
 
     let num1 = lines[1].trim().split("").reverse();
     let num2 = lines[2].substr(1).trim().split("").reverse();
-    let carry = [];
-    for (let i = 0; i < base + 100; i++) {
-        carry[i] = 0;
-    }
+    let carry = 0;
 
     let result = "";
     let lastIndex = 0;
@@ -18,10 +15,12 @@ function processData(input) {
         num1.forEach((item, index) => {
             let add1 = chars.indexOf(item);
             let add2 = index < num2.length ? chars.indexOf(num2[index]) : 0;
-            let res = add1 + add2 + carry[index];
+            let res = add1 + add2 + carry;
             if (res >= base) {
-                carry[index + 1] = Math.floor(res/base);
+                carry = Math.floor(res / base);
                 res %= base;
+            } else {
+                carry = 0;
             }
             result += chars.charAt(res);
             lastIndex++;
@@ -30,10 +29,12 @@ function processData(input) {
         num2.forEach((item, index) => {
             let add1 = chars.indexOf(item);
             let add2 = index < num1.length ? chars.indexOf(num1[index]) : 0;
-            let res = add1 + add2 + carry[index];
+            let res = add1 + add2 + carry;
             if (res >= base) {
-                carry[index + 1] = Math.floor(res/base);
+                carry = Math.floor(res / base);
                 res %= base;
+            } else {
+                carry = 0;
             }
             result += chars.charAt(res);
             lastIndex++;
@@ -41,8 +42,8 @@ function processData(input) {
 
     }
 
-    if (carry[lastIndex] > 0) {
-        result += chars.charAt(carry[lastIndex]);
+    if (carry > 0) {
+        result += chars.charAt(carry);
     }
 
     result = result.split("").reverse().join("");
@@ -51,9 +52,8 @@ function processData(input) {
     console.log(lines[1]);
     console.log(lines[2]);
     console.log(lines[3]);
-    console.log((carry[lastIndex] > 0 ? "" : " ") + result);
+    console.log((carry > 0 ? "" : " ") + result);
 }
-
 
 var fs = require('fs');
 
