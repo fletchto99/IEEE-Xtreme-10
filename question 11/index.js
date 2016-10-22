e641966683d3380c24b48a0ecee4c5ebc3ff5048
Matt Langlois <fletchto99@gmail.com>
2016-10-22 11:59:23 -0400

    Optimizations but it still doesn't work correctly!! Why?

--- question 11/index.js
+++ question 11/index.js
@@ -8,37 +8,36 @@ function processData(input) {
         let args = item.split(" ").map(item => parseInt(item));
 
         args[1] = args[1] - 1;
-        args[2] = args[2];
-        args[3] = args[3] -1;
+        args[2] = args[2] - 1;
+        args[3] = args[3] - 1;
 
         if (args[0] == 1) {
-            let sub1 = password.substr(args[1], args[2] - args[1]);
-            let sub2 = password.substr(args[3], (args[2] - args[1]));
-            // console.log("comparing" + sub1 + " to " + sub2);
+            let sub1 = password.substr(args[1], args[2] - args[1] + 1);
+            let sub2 = password.substr(args[3], (args[2] - args[1] + 1));
             console.log(sub1 == sub2 ? 'Y' : 'N');
         } else if (args[0] == 2) {
-            let replaceValue = password.substr(args[3], (args[2] - args[1]));
-            let index = 0;
-
-            for (let i = args[1]; i < args[2]; i++) {
-                password = password.replaceAt(i, replaceValue.charAt(index++));
-            }
-
+            password = password.replaceBetween(args[1], args[2] + 1, password.substr(args[3], (args[2] - args[1] + 1)));
         } else if (args[0] == 3) {
-            for (let i = args[1]; i < args[2]; i++) {
+
+            let replacement = "";
+
+            for (let i = args[1]; i <= args[2]; i++) {
                 if (password.charAt(i) == 'z') {
-                    password = password.replaceAt(i, 'a')
+                    replacement += 'a';
+                } else if (password.charAt(i) == 'Z') {
+                    replacement += 'Z';
                 } else {
-                    password = password.replaceAt(i, String.fromCharCode(password.charCodeAt(i) + 1));
+                    replacement += String.fromCharCode(password.charCodeAt(i) + 1);
                 }
             }
+            password = password.replaceBetween(args[1], args[2] + 1, replacement);
         }
     })
 }
 
-String.prototype.replaceAt = function (index, character) {
-    return this.substr(0, index) + character + this.substr(index + 1);
-}
+String.prototype.replaceBetween = function (start, end, what) {
+    return this.substring(0, start) + what + this.substring(end);
+};
 
 
 var fs = require('fs');
